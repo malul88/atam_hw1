277440dbb8b309b24b32447c524de8c5d6278517
student <Michael Malul>
2021-04-19 12:51:39 +0000

    ex2 done

--- ex2.asm
+++ ex2.asm
@@ -2,4 +2,16 @@
 
 .section .text
 _start:
-#your code here
+        movq $source , %rax
+        movq $destination, %rbx
+        movl num, %ecx
+        xor %dx, %dx
+loop1:  cmp $0x0, %ecx
+        je end
+        movb (%rax) ,%dl
+        movb %dl, (%rbx)
+        add $0x1, %rbx
+        dec %ecx
+        add $0x1, %rax
+        jmp loop1
+end:    
