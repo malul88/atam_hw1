98f04155c622a3960096a44e863feebb5414f6e8
Migraine7 <62520123+Migraine7@users.noreply.github.com>
2021-05-20 03:25:54 +0300

    changed to dwords, not working yet

--- hw2/part1/matrix.asm
+++ hw2/part1/matrix.asm
@@ -6,7 +6,7 @@ get_elemnt_from_matrix: # rdi - &matrix, esi - n, edx - row, ecx - col
 	# epilogue
 	pushq %rbp
 	movq %rsp, %rbp
-
+	
 	# body
 	xorq %rax, %rax
 	xorq %r11, %r11
@@ -27,58 +27,58 @@ multiplyMatrices:
 	# epilogue
 	pushq %rbp
 	movq %rsp, %rbp
-	subq $64, %rsp # for local vars
+	subq $44, %rsp # for local vars
 	
 	# save local vars
 	movq %rdi, -8(%rbp) # &first
 	movq %rsi, -16(%rbp) # &second
 	movq %rdx, -24(%rbp) # &result
-	movq %rcx, -32(%rbp) # m (row of first)
-	movq %r8, -40(%rbp) # n (col of first)
-	movq %r9, -48(%rbp) # r (col of second)
-	movq $0, -56(%rbp) # index i
-	movq $0, -64(%rbp) # index j
+	movl %ecx, -28(%rbp) # m (row of first)
+	movl %r8d, -32(%rbp) # n (col of first)
+	movl %r9d, -36(%rbp) # r (col of second)
+	movl $0, -40(%rbp) # index i
+	movl $0, -44(%rbp) # index j
 	# 16(rbp) = p (p of Zp)
 
 	# body
 	loop_i:
 		# check i == m
-		movq -32(%rbp), %rdx
-		cmp -56(%rbp), %rdx
+		movl -28(%rbp), %edx # m
+		cmp -40(%rbp), %edx # i
 		je end_loop_i
 
 		loop_j:
 			# check j == r
-			movq -64(%rbp), %rdx
-			cmp -48(%rbp), %rdx
+			movl -44(%rbp), %edx # j
+			cmp -36(%rbp), %edx # r
 			je end_loop_j
 
-			movq -8(%rbp), %rdi
-			movq -16(%rbp), %rsi
-			movq -40(%rbp), %rdx
-			movq -48(%rbp), %rcx
-			movq -56(%rbp), %r8
-			movq -64(%rbp), %r9
+			movq -8(%rbp), %rdi # &first
+			movq -16(%rbp), %rsi # &second
+			movl -32(%rbp), %edx # n
+			movl -36(%rbp), %ecx # r
+			movl -40(%rbp), %r8d # i
+			movl -44(%rbp), %r9d # j
 			call mulRowByCol
-			# rax now hold the result for rowAi*ColBj
+			# eax now hold the result for rowAi*ColBj
 
 			xorq %rdx, %rdx
-			movq 16(%rbp), %rcx
-			div %rcx # rdx:rax/p
+			movl 16(%rbp), %ecx # p
+			div %ecx # edx:eax/p
 			
-			# change result[i,j]  to rdx
-			movq %rdx, %r8 # rdx - value
+			# change result[i,j]  to edx
+			movl %edx, %r8d # edx - value
 			movq -24(%rbp), %rdi # &result
-			movq -48(%rbp), %rsi # r - col of result
-			movq -56(%rbp), %rdx # i - index of row
-			movq -64(%rbp), %rcx # j - index of col
+			movl -36(%rbp), %esi # r - col of result
+			movl -40(%rbp), %edx # i - index of row
+			movl -44(%rbp), %ecx # j - index of col
 			call set_elemnt_in_matrix
 
-			incq -64(%rbp)
+			incl -44(%rbp) # j
 			jmp loop_j
 		
 		end_loop_j:
-			incq -56(%rbp)
+			incl -40(%rbp) # i
 			jmp loop_i
 	
 	end_loop_i:
@@ -90,47 +90,51 @@ mulRowByCol: # int mulRowCol(int* mat1, int* mat2, int n, int r, int i, int j)
 	# epilogue
 	pushq %rbp
 	movq %rsp, %rbp
-	subq $56, %rsp
+	subq $36, %rsp
 
 	# save local vars
 	movq %rdi, -8(%rbp) # &first
 	movq %rsi, -16(%rbp) # &second
-	movq %rdx, -24(%rbp) # n - col of first
-	movq %rcx, -32(%rbp) # r - col of second
-	movq %r8, -40(%rbp) # i - row of first
-	movq %r9, -48(%rbp) # j - row of second
-	movq $0, -56(%rbp) # k - index
+	movl %edx, -20(%rbp) # n - col of first
+	movl %ecx, -24(%rbp) # r - col of second
+	movl %r8d, -28(%rbp) # i - row of first
+	movl %r9d, -32(%rbp) # j - row of second
+	movl $0, -36(%rbp) # k - index
 	
 	xorq %r11, %r11
 
 	# save local vars
 	loop_k:
 		# check k == n
-		movq -56(%rbp), %rdx
-		cmp -24(%rbp), %rdx
+		movl -36(%rbp), %edx # k
+		cmp -20(%rbp), %edx # n
 		je end_loop_k
 
 		movq -8(%rbp), %rdi # &first
-		movq -24(%rbp), %rsi # n
-		movq -40(%rbp), %rdx # i
-		movq -56(%rbp), %rcx # k
+		movl -20(%rbp), %esi # n
+		movl -28(%rbp), %edx # i
+		movl -36(%rbp), %ecx # k
+		pushq %r11
 		call get_elemnt_from_matrix
-		movq %rax, %r10 # r10 = mat1[i,k]
+		popq %r11
+		movl %eax, %r10d # r10d = mat1[i,k]
 
 		movq -16(%rbp), %rdi # &second
-		movq -32(%rbp), %rsi # r
-		movq -56(%rbp), %rdx # k
-		movq -48(%rbp), %rcx # j
+		movl -24(%rbp), %esi # r
+		movl -36(%rbp), %edx # k
+		movl -32(%rbp), %ecx # j
+		pushq %r11
 		call get_elemnt_from_matrix
+		popq %r11
 		
-		mul %r10 # rdx:rax = mat1[i,k]*mat2[k,j]
-		add %rax, %r11
+		mul %r10d # edx:eax = mat1[i,k]*mat2[k,j]
+		add %eax, %r11d
 		
-		incq -56(%rbp)
+		incl -36(%rbp) # k
 		jmp loop_k
 	
 	end_loop_k:
-		movq %r11, %rax
+		movl %r11d, %eax
 	
 	# prologue
 	leave
